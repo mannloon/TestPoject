74ffb0e985af25f6536960b946b09f57670b5e21
Adam R Mann <armann@mmiemail.com>
2013-06-25 09:34:49 -0500

    Test Script
    Yo momma is so fat, She's at risk for type 2 diabetes.

---
+++ Assets/AScript.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+
+public class AScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+		//Yo momma is so fat, She's at risk for type 2 diabetes.
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
